a056a7a1cd08dba63240924cb26af081a7a574bb
mominalvi <mominalvi30@gmail.com>
2024-08-04 05:52:41 -0400

    Update main page

---
+++ client/website/app/components/head.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+ 
+function IndexPage() {
+  return (
+    <div>
+      <Head>
+        <title className='text-black'>test</title>
+      </Head>
+    </div>
+  )
+}
+ 
+export default IndexPage
--- client/website/app/page.tsx
+++ client/website/app/page.tsx
@@ -6,31 +6,39 @@ import IntroBox from "./components/intro-box";
 import Recent from "./components/recent";
 import Saved from "./components/saved";
 import TopMatches from "./components/top-matches";
-import Link from 'next/link';
 import React from "react";
+import Head from 'next/head'
+
 
 export default function Home() {
   return (
-    <main className="bg-white min-h-screen">
-      <Navbar />
-      <IntroBox />
+    <>
+      <Head>
+        <title>test</title>
+      </Head>
+      
+      <main className="bg-white min-h-screen">
+          <Navbar />
+          <IntroBox />
 
-      <div className="flex flex-row space-x-5 items-start px-20 pt-12">
-        <div className="bg-white rounded-full border-2 border-[#f0f0f0] px-8 py-2 inline-block hover:bg-[#73AAE8] group">
-          <h1 className="text-black font-spaceGrotesk group-hover:text-[#1E5A9E]">View Matches</h1>
-        </div>
-          <div className="bg-white rounded-full border-2 border-[#f0f0f0] px-8 py-2 inline-block hover:bg-[#73AAE8] group">
-            <h1 className="text-black font-spaceGrotesk group-hover:text-[#1E5A9E]">Change Preferences</h1>
+          <div className="flex flex-row space-x-5 items-start px-20 pt-12">
+            <div className="bg-white rounded-full border-2 border-[#f0f0f0] px-8 py-2 inline-block hover:bg-[#73AAE8] group">
+              <h1 className="text-black font-spaceGrotesk group-hover:text-[#1E5A9E]">View Matches</h1>
+            </div>
+            <div className="bg-white rounded-full border-2 border-[#f0f0f0] px-8 py-2 inline-block hover:bg-[#73AAE8] group">
+              <h1 className="text-black font-spaceGrotesk group-hover:text-[#1E5A9E]">Change Preferences</h1>
+            </div>
           </div>
-        </div>
 
 
-      <TopMatches />
-      <Saved />
-      <Recent />
-    </main>
+          <TopMatches />
+          <Saved />
+          <Recent />
+        </main>
+    </>
 
   );
+
 }
 
 
